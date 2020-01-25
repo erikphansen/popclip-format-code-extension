aef4017fecbee0d95156fa6d5782f1c047c01928
Erik P. Hansen <eph@erikphansen.com>
2020-01-25 14:17:19 -0800

    Revise the JSON extension
    - Make file names more generic
    - Make reading user's prettier settings actually work

--- format-json-rollup.config.js
+++ format-json-rollup.config.js
@@ -2,11 +2,12 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs'
 
 module.exports = {
-  input: 'src/main.js',
+  input: 'src/main-json.js',
   output: {
-    file: 'Format JSON.popclipext/format-json.js',
+    file: 'Format JSON.popclipext/main.js',
     format: 'cjs',
     compact: true,
   },
+  external: ['fs', 'os'],
   plugins: [resolve(), commonjs()],
 };
---
+++ src/main-json.js
@@ -0,0 +1,21 @@
+import fs from 'fs';
+import os from 'os';
+import prettier from 'prettier/standalone';
+import jsParsers from 'prettier/parser-babylon';
+
+var input = process.env.POPCLIP_TEXT;
+
+let prettierrc = {};
+
+try {
+  const prettierConfigPath = os.homedir() + '/.prettierrc';
+  prettierrc = JSON.parse(fs.readFileSync(prettierConfigPath, 'utf8'));
+} catch (error) {}
+
+const formatted = prettier.format(input + '\n', {
+  ...prettierrc,
+  parser: 'json',
+  plugins: [jsParsers],
+});
+
+console.log(formatted.trim());
